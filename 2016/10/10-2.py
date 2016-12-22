ecaf142b14233c1ac33313e91bbead610f6b0668
Eugene Ng <eugenen@utexas.edu>
2016-12-21 22:27:04 -0600

    finished 10

--- 2016/10/10-2.py
+++ 2016/10/10-2.py
@@ -1,5 +1,5 @@
 """
-Given a list of microchip and bot instructions, find which bot is the one to compare two certain microchips
+Given a list of microchip and bot instructions, multiply the values of one chip in each of the outputs 0, 1, and 2
 """
 
 #!/usr/bin/env python3
@@ -22,14 +22,12 @@ outputs = [[] for i in range(21)] # hardcoding in the number of outputs
 # run
 # ---
 
-def run(chip_val1, chip_val2):
+def run():
 	"""
-	transfer chips according to the instructions until a bot has both chips with the values that were passed in; return that bot
+	transfer chips according to the instructions until no bot has 2 chips
 	"""
 	full_bots = list(filter(lambda x: len(x) == 2, bots))
 	while full_bots:
-		if {chip_val1, chip_val2} in full_bots:
-			return bots.index({chip_val1, chip_val2})
 		for chips in full_bots:
 			bot = bots.index(chips)
 			if give_tos[bot][1] == 'bot':
@@ -42,7 +40,6 @@ def run(chip_val1, chip_val2):
 				outputs[give_tos[bot][2]].append(max(chips))
 			bots[bot].clear()
 		full_bots = list(filter(lambda x: len(x) == 2, bots))
-	return -1
 
 # ---------
 # add_instr
@@ -73,8 +70,8 @@ def solve(reader, writer):
 	# also fill a list of bots and their chip slots
 	for line in reader:
 		add_instr(line)
-	out = str(run(61, 17))
-	writer.write(out)
+	run()
+	writer.write(str(outputs[0][0] * outputs[1][0] * outputs[2][0]))
 
 # ----
 # main
