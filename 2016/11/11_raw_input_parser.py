7970b57967c53a6806c4ca35f27d254f491f9365
Eugene Ng <eugenen@utexas.edu>
2016-12-26 04:24:02 -0600

    continued 11, need to implement predicates "above" and "element"

--- 2016/11/11_raw_input_parser.py
+++ 2016/11/11_raw_input_parser.py
@@ -11,14 +11,29 @@ Given a description of the locations of the generators and the microchips, form 
 import sys
 from element_symbols_dict import element_symbols
 
-# ----
-# read
-# ----
+# -----
+# parse
+# -----
 
-def read(line):
+def parse(line, writer):
 	"""
-	do stuff
+	each line will contain information about which floor its describing and what objects are on that floor initially
+	parse the line to form more clingo-friendly facts
 	"""
+	# NOTE: eventually want to have some way to indicate the order of the floors, like an 'above' predicate or something
+	# ALSO: have to be able to associate generators with their matching microchips
+	words = line.split()
+	floor = words[1]
+	writer.write('loc(' + floor + ').\n')
+	for i in range(2, len(words)):
+		if 'generator' in words[i]:
+			item = element_symbols[words[i - 1]] + 'G'
+			writer.write('generator(' + item + ').\n')
+			writer.write('on(' + item + ', ' + floor + ', 0).\n')
+		if 'microchip' in words[i]:
+			item = element_symbols[words[i - 1].replace('-compatible', '')] + 'M'
+			writer.write('microchip(' + item + ').\n')
+			writer.write('on(' + item + ', ' + floor + ', 0).\n')
 
 # ----------
 # form_input
@@ -30,8 +45,9 @@ def form_input(reader, writer):
 	writer a writer
 	"""
 	for line in reader:
-		# do stuff
-	writer.write()
+		parse(line, writer)
+	# assuming the first floor is always called 'first', the elevator will always start out on 'first'
+	writer.write('elevator(first, 0).')
 
 # ----
 # main
