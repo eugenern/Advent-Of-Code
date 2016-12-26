735a30798ab720ecca5a70446fea4f016eee5b32
Eugene Ng <eugenen@utexas.edu>
2016-12-26 02:13:28 -0600

    started 11

---
+++ 2016/11/11_raw_input_parser.py
@@ -0,0 +1,41 @@
+"""
+Given a description of the locations of the generators and the microchips, form the input to solve the problem in clingo syntax
+"""
+
+#!/usr/bin/env python3
+
+# -------
+# imports
+# -------
+
+import sys
+from element_symbols_dict import element_symbols
+
+# ----
+# read
+# ----
+
+def read(line):
+	"""
+	do stuff
+	"""
+
+# ----------
+# form_input
+# ----------
+
+def form_input(reader, writer):
+	"""
+	reader a reader
+	writer a writer
+	"""
+	for line in reader:
+		# do stuff
+	writer.write()
+
+# ----
+# main
+# ----
+
+if __name__ == "__main__":
+	form_input(sys.stdin, sys.stdout)
