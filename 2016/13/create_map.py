5796005f9fbd6f5eab48074a529f730ffc48eb18
Eugene Ng <eugenen@utexas.edu>
2016-12-30 05:02:15 -0600

    did 12, barely started 13, will continue on 11

---
+++ 2016/13/create_map.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# input
+fav_num = 1364
+
+# 100x100 is just a guess for the maximum area needed; may not allow globally optimal solution to be found
+area = [['.'] * 100 for i in range(100)]
+
+for y in range(len(area)):
+	for x in range(len(area[0])):
+		if bin(x*x + 3*x + 2*x*y + y + y*y + fav_num).count('1') % 2:
+			area[y][x] = '#'
+
+# goal
+area[39][31] = '*'
+
+for row in area:
+	for loc in row:
+		print(loc, end='')
+	print()
