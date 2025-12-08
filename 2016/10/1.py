a545d9960f2cab27cf6c087e390095e87ab05381
Eugene <hoihinsauce@gmail.com>
2025-12-08 13:39:28 -0800

    somewhat improve code of 2016/10 solns

--- 2016/10/1.py
+++ 2016/10/1.py
@@ -16,11 +16,11 @@ import sys
 # -----------
 
 # hardcoding in the number of bots
-bots = [set() for i in range(210)]
-# lists should be in format [give low to, bot or output, give high to, bot or output]
-give_tos = [[] for i in range(210)]
+bots = [set() for _ in range(210)]
+# lists should be in format [bot or output, give low to, bot or output, give high to]
+give_tos = [[] for _ in range(210)]
 # hardcoding in the number of outputs
-outputs = [[] for i in range(21)]
+outputs = [[] for _ in range(21)]
 
 # ---
 # run
@@ -31,22 +31,21 @@ def run(chip_val1, chip_val2):
     transfer chips according to the instructions
     until a bot has both chips with the values that were passed in; return that bot
     """
-    full_bots = list(filter(lambda x: len(x) == 2, bots))
-    while full_bots:
-        if {chip_val1, chip_val2} in full_bots:
-            return bots.index({chip_val1, chip_val2})
-        for chips in full_bots:
-            bot = bots.index(chips)
-            if give_tos[bot][1] == 'bot':
-                bots[give_tos[bot][0]].add(min(chips))
-            else:
-                outputs[give_tos[bot][0]].append(min(chips))
-            if give_tos[bot][3] == 'bot':
-                bots[give_tos[bot][2]].add(max(chips))
-            else:
-                outputs[give_tos[bot][2]].append(max(chips))
-            bots[bot].clear()
-        full_bots = list(filter(lambda x: len(x) == 2, bots))
+    while any(len(bot) > 1 for bot in bots):
+        for i, bot in enumerate(bots):
+            if len(bot) > 1:
+                if chip_val1 in bot and chip_val2 in bot:
+                    return i
+                lo_dest_type, lo_dest_ind, hi_dest_type, hi_dest_ind = give_tos[i]
+                if lo_dest_type == 'bot':
+                    bots[lo_dest_ind].add(min(bot))
+                else:
+                    outputs[lo_dest_ind].append(min(bot))
+                if hi_dest_type == 'bot':
+                    bots[hi_dest_ind].add(max(bot))
+                else:
+                    outputs[hi_dest_ind].append(max(bot))
+                bot.clear()
     return -1
 
 # ---------
@@ -60,7 +59,7 @@ def add_instr(instr):
     words = instr.split()
     if words[0] == 'bot':
         # add to bot instr list
-        give_tos[int(words[1])] = [int(words[6]), words[5], int(words[11]), words[10]]
+        give_tos[int(words[1])] = [words[5], int(words[6]), words[10], int(words[11])]
     else:
         # give bot in bot list the chip
         bots[int(words[5])].add(int(words[1]))
@@ -79,8 +78,7 @@ def solve(reader, writer):
     # also fill a list of bots and their chip slots
     for line in reader:
         add_instr(line)
-    out = str(run(61, 17))
-    writer.write(out)
+    writer.write(str(run(61, 17)))
 
 # ----
 # main
--- 2016/10/2.py
+++ 2016/10/2.py
@@ -16,11 +16,11 @@ import sys
 # -----------
 
 # hardcoding in the number of bots
-bots = [set() for i in range(210)]
-# lists should be in format [give low to, bot or output, give high to, bot or output]
-give_tos = [[] for i in range(210)]
+bots = [set() for _ in range(210)]
+# lists should be in format [bot or output, give low to, bot or output, give high to]
+give_tos = [[] for _ in range(210)]
 # hardcoding in the number of outputs
-outputs = [[] for i in range(21)]
+outputs = [[] for _ in range(21)]
 
 # ---
 # run
@@ -30,20 +30,19 @@ def run():
     """
     transfer chips according to the instructions until no bot has 2 chips
     """
-    full_bots = list(filter(lambda x: len(x) == 2, bots))
-    while full_bots:
-        for chips in full_bots:
-            bot = bots.index(chips)
-            if give_tos[bot][1] == 'bot':
-                bots[give_tos[bot][0]].add(min(chips))
-            else:
-                outputs[give_tos[bot][0]].append(min(chips))
-            if give_tos[bot][3] == 'bot':
-                bots[give_tos[bot][2]].add(max(chips))
-            else:
-                outputs[give_tos[bot][2]].append(max(chips))
-            bots[bot].clear()
-        full_bots = list(filter(lambda x: len(x) == 2, bots))
+    while any(len(bot) > 1 for bot in bots):
+        for i, bot in enumerate(bots):
+            if len(bot) > 1:
+                lo_dest_type, lo_dest_ind, hi_dest_type, hi_dest_ind = give_tos[i]
+                if lo_dest_type == 'bot':
+                    bots[lo_dest_ind].add(min(bot))
+                else:
+                    outputs[lo_dest_ind].append(min(bot))
+                if hi_dest_type == 'bot':
+                    bots[hi_dest_ind].add(max(bot))
+                else:
+                    outputs[hi_dest_ind].append(max(bot))
+                bot.clear()
 
 # ---------
 # add_instr
@@ -56,7 +55,7 @@ def add_instr(instr):
     words = instr.split()
     if words[0] == 'bot':
         # add to bot instr list
-        give_tos[int(words[1])] = [int(words[6]), words[5], int(words[11]), words[10]]
+        give_tos[int(words[1])] = [words[5], int(words[6]), words[10], int(words[11])]
     else:
         # give bot in bot list the chip
         bots[int(words[5])].add(int(words[1]))
