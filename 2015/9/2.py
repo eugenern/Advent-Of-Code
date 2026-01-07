54f2c7abb4d455b6f19ae3cfe3a037f367b9b56a
Eugene <hoihinsauce@gmail.com>
2026-01-06 18:44:42 -0800

    inefficiently solve 2015 day 9

---
+++ 2015/9/2.py
@@ -0,0 +1,76 @@
+"""
+Given a list of distances between locations, calculate the total distance of the longest route
+to visit every location exactly once
+"""
+
+# -------
+# imports
+# -------
+
+import sys
+from itertools import permutations
+
+# --------------
+# form_dist_dict
+# --------------
+
+def form_dist_dict(reader):
+    """
+    Given list of distances between locations, for a dict mapping start loc to a dict that maps
+    dest locs to distances
+    """
+    dist_dict = {}
+    for line in reader:
+        words = line.split()
+        loc_1, loc_2, dist = words[0], words[2], int(words[-1])
+        if loc_1 not in dist_dict:
+            dist_dict[loc_1] = {}
+        if loc_2 not in dist_dict:
+            dist_dict[loc_2] = {}
+        dist_dict[loc_1][loc_2] = dist
+        dist_dict[loc_2][loc_1] = dist
+
+    return dist_dict
+
+# -----
+# solve
+# -----
+
+def solve(reader):
+    """
+    Use distances between locations to find longest route to visit every location once
+    """
+    # idea: form a dict start loc -> dict dest loc -> distance
+    # remember to set dict item in both directions
+
+    # also, during the search (BFS or DFS?) keep track of most total distance found so far
+    # and cut a search short if the distance becomes too far
+
+    # alternate idea: instead of BFS or DFS, iterate thru all permutations of all locations
+    # i guess that would involve a lot of recalculations tho
+
+    # another idea: instead of 8! permutations, fix a starting loc and for each of the 7!
+    # permutations of routes from there, subtract the least distance in the path
+
+    dist_dict = form_dist_dict(reader)
+    # for k, v in dist_dict.items():
+    #     print(k)
+    #     for k1, v1 in v.items():
+    #         print(f'\tdest {k1} dist {v1}')
+
+    best_found = 0
+    for route in permutations(dist_dict.keys()):
+        route_dist = 0
+        for i, loc in enumerate(route[:-1]):
+            route_dist += dist_dict[loc][route[i + 1]]
+        if route_dist > best_found:
+            best_found = route_dist
+
+    return best_found
+
+# ----
+# main
+# ----
+
+if __name__ == "__main__":
+    print(solve(sys.stdin))
